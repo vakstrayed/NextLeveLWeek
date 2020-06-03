dc0e97eae35dbd451af9967a6c171c4c7b41bc22
Mateus Silva <a.mateuslol@gmail.com>
2020-06-03 01:04:05 -0300

    day1

---
+++ server/src/routes.ts
@@ -0,0 +1,51 @@
+
+import express from 'express';
+import knex from './database/connection'
+
+const routes = express.Router();
+
+routes.get('/items', async (request,response) => {
+    const items = await knex('items').select('*');
+
+    const serializedItems = items.map(item => {
+        return {
+            id: item.id,
+            title: item.name,
+            image_url: `http://localhost:3333/uploads/${item.image}`,
+        };
+    });
+
+    return response.json(items);
+});
+
+routes.get('/teste', (request,response) => {
+    return 1;
+});
+
+routes.post('/points', async (request,response) => {
+    const {
+        name,
+        email,
+        whatsapp,
+        latitude,
+        longitude,
+        city,
+        uf,
+        items
+    } = request.body;
+
+    await knex('points').insert({
+        image: 'default',
+        name,
+        email,
+        whatsapp,
+        latitude,
+        longitude,
+        city,
+        uf
+    });
+
+    return response.json({success: true});
+});
+
+export default routes;
